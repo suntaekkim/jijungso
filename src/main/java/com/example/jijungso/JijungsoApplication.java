7f9ee4be4daa73fdcd8afa1171d6a0af1a4f74ff
kst2307 <kst2307@hanmail.net>
2021-03-05 23:03:35 +0900

    Initial commit

---
+++ src/main/java/com/example/jijungso/JijungsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jijungso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JijungsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JijungsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jijungso/JijungsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jijungso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JijungsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
